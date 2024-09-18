95e89b76d6f5e19bbcdb49c0b59f4c99ad39cba7
jhongoln <jhongoln@suramericana.com.co>
2024-09-17 21:14:28 -0500

    firts commit

---
+++ src/main/java/com/mycv/SpringBootCV/SpringBootCvApplication.java
@@ -0,0 +1,13 @@
+package com.mycv.SpringBootCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycv/SpringBootCV/SpringBootCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycv.SpringBootCV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
